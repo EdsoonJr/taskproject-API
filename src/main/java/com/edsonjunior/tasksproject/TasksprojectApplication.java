f1dd1321a370bf9bd9b95e242fb76b3b7966a2bc
Edson Junior <edsonjn2013@gmail.com>
2023-07-11 23:08:35 -0300

    first commit

---
+++ src/main/java/com/edsonjunior/tasksproject/TasksprojectApplication.java
@@ -0,0 +1,13 @@
+package com.edsonjunior.tasksproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edsonjunior/tasksproject/TasksprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edsonjunior.tasksproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
